7e5ea733c5d5afc5325e54ba88f2fb9755eeba01
Noel Lai <noel@meepshop.com>
2015-06-30 17:05:31 +0800

    Long time no ci

--- main.js
+++ main.js
@@ -4,6 +4,8 @@
 
 /* nexmo --------------------------- */
 var nexmoAPI = require('cloud/lib/nexmoKey');
+var categoryConstants = require('cloud/lib/categoryConstants');
+var Image = require("parse-image");
 
 Parse.Cloud.define("verifyRequest", function (request, response) {
   Parse.Cloud.httpRequest({
@@ -52,42 +54,82 @@ Parse.Cloud.define("verifyCheck", function (request, response) {
   });
 });
 
-Parse.Cloud.define('findFriendByPhones', function (request, response) {
-  var promiseReqs = request.params.phones.map(function (phone) {
-    var query = new Parse.Query(Parse.User);
-    query.equalTo('mobile', phone);
-    return query.first();
-  });
+Parse.Cloud.beforeSave("_User", function(request, response) {
+  console.log('beforeSave User');
+  var user = request.object;
+  console.log('user: ', user);
+  if (!user.dirty("avatarImage")) {
+    // The profile photo isn't being modified.
+    console.log('no modified');
+    response.success();
+  } else {
+    console.log('modified');
+    Parse.Cloud.httpRequest({
+      url: user.get("avatarImage").url()
 
-  var promise = new Parse.Promise();
-  Parse.Promise.when(promiseReqs).then(function () {
-    var users = [].slice.call(arguments);
-    users = users.filter(function (user) {
-      return user && user.id !== request.user.id;
+    }).then(function(response) {
+      var image = new Image();
+      return image.setData(response.buffer);
+
+    }).then(function(image) {
+      // Crop the image to the smaller of width or height.
+      var size = Math.min(image.width(), image.height());
+      return image.crop({
+        left: (image.width() - size) / 2,
+        top: (image.height() - size) / 2,
+        width: size,
+        height: size
+      });
+
+    }).then(function(image) {
+      // Resize the image to 64x64.
+      return image.scale({
+        width: 200,
+        height: 200
+      });
+
+    }).then(function(image) {
+      // Make sure it's a JPEG to save disk space and bandwidth.
+      return image.setFormat("PNG");
+
+    }).then(function(image) {
+      // Get the image data in a Buffer.
+      return image.data();
+
+    }).then(function(buffer) {
+      // Save the image into a new file.
+      var base64 = buffer.toString("base64");
+      var cropped = new Parse.File("thumbnail.png", { base64: base64 });
+      return cropped.save();
+
+    }).then(function(cropped) {
+      // Attach the image file to the original object.
+      user.set("avatarImage", cropped);
+
+    }).then(function(result) {
+      response.success();
+    }, function(error) {
+      response.error(error);
     });
-    request.user.relation('friendsRelation').add(users);
-    request.user.save().then(function () {
-      response.success('ok');
-    });
-  });
+  }
 });
 
 Parse.Cloud.afterDelete('Products', function (request) {
   var productId = request.object.id;
   var activitesQuery = new Parse.Query('Activites');
-  activitesQuery.equalTo('activityObject', request.object).find(function (activites) {
+  activitesQuery.equalTo('activityObject', request.object).find().then(function (activites) {
     Parse.Object.destroyAll(activites);
   });
   var messagesQuery = new Parse.Query('Messages');
-  messagesQuery.equalTo('product', request.object).find(function (messages){
+  messagesQuery.equalTo('product', request.object).find().then(function (messages){
     Parse.Object.destroyAll(messages);
   });
   var commentsQuery = new Parse.Query('Comments');
-  commentsQuery.equalTo('product', request.object).find(function (comments) {
+  commentsQuery.equalTo('product', request.object).find().then(function (comments) {
     Parse.Object.destroyAll(comments);
   });
   var likesQuery = new Parse.Query('Likes');
-  likesQuery.equalTo('likedProduct', request.object).find(function (likes) {
+  likesQuery.equalTo('likedProduct', request.object).find().then(function (likes) {
     Parse.Object.destroyAll(likes);
   });
 
@@ -132,6 +174,25 @@ Parse.Cloud.afterDelete('Messages', function (request) {
   chatsQuery.equalTo('chatRoomId', request.object.chatRoomId);
 });
 
+Parse.Cloud.define('findFriendByPhones', function (request, response) {
+  var promiseReqs = request.params.phones.map(function (phone) {
+    var query = new Parse.Query(Parse.User);
+    query.equalTo('mobile', phone);
+    return query.first();
+  });
+
+  Parse.Promise.when(promiseReqs).then(function () {
+    var users = [].slice.call(arguments);
+    users = users.filter(function (user) {
+      return user && user.id !== request.user.id;
+    });
+    request.user.relation('friendsRelation').add(users);
+    request.user.save().then(function () {
+      response.success('ok');
+    });
+  });
+});
+
 Parse.Cloud.define('getProductById', function (request, response) {
   var productsQuery = new Parse.Query('Products');
   var commentsQuery = new Parse.Query('Comments');
@@ -140,8 +201,8 @@ Parse.Cloud.define('getProductById', function (request, response) {
   productContainer.liked = false;
   productsQuery.include('seller');
   productsQuery.get(request.params.productId).then(function (product) {
-    var commentsPromise = commentsQuery.equalTo('product', product).find()
-    var likesPromise = likesQuery.equalTo('likedProduct', product).find()
+    var commentsPromise = commentsQuery.equalTo('product', product).include('commenter').descending('createdAt').find()
+    var likesPromise = likesQuery.equalTo('likedProduct', product).include('likedUser').find()
     productContainer.product = product;
     return Parse.Promise.when([commentsPromise, likesPromise])
   }).then(function (comments, likes) {
@@ -157,19 +218,53 @@ Parse.Cloud.define('getProductById', function (request, response) {
 })
 
 Parse.Cloud.define('getProductsByUser', function (request, response) {
-  var userId = request.params.userId;
+  var user = request.user;
   var productsQuery = new Parse.Query('Products');
   var commentsQuery = new Parse.Query('Comments');
   var likesQuery = new Parse.Query('Likes');
-  productsQuery.equalTo('seller', userId);
+  productsQuery.equalTo('seller', user);
   productsQuery.find().then(function (products) {
     response.success(products);
   })
 });
 
+Parse.Cloud.define('getLikedProductsByUser', function (request, response) {
+  var user = request.user;
+  var likesQuery = new Parse.Query('Likes');
+  likesQuery.equalTo('likedUser', user);
+  likesQuery.include('likedProduct').find().then(function (likes) {
+    console.log('likes: ', likes);
+    var likedProducts = likes.map(function (like) {
+      return like.get('likedProduct');
+    });
+    response.success(likedProducts);
+  })
+});
+
+Parse.Cloud.define('unlikeProducts', function (request, response) {
+  console.log('unlikeProducts: ' + request.params.unlikedProductId);
+  var unlikedProductId = request.params.unlikedProductId || 0;
+  var user = request.user;
+  var likesQuery = new Parse.Query('Likes');
+  var productsQuery = new Parse.Query('Products');
+  productsQuery.get(request.params.unlikedProductId).then(function (product) {
+    likesQuery
+      .equalTo('likedUser', user)
+      .equalTo('likedProduct', product)
+      .find().then(function (likes) {
+        console.log('likes: ', likes);
+        Parse.Object.destroyAll(likes);
+        response.success('unlikeProducts OK');
+    });
+  })
+});
+
+
 Parse.Cloud.define('productWithRelated', function (request, response) {
   var skip = request.params.skip || 0;
   var limit = request.params.limit || 10;
+  var selectedCategory = request.params.selectedCategory;
+  console.log('selectedCategory: ' + selectedCategory);
   var productsQuery = new Parse.Query('Products');
 
   productsQuery
@@ -177,18 +272,26 @@ Parse.Cloud.define('productWithRelated', function (request, response) {
     .skip(skip)
     .include('seller')
     .descending('createdAt');
+  if (typeof selectedCategory !== "undefined" && selectedCategory !== categoryConstants.ALL_PRODUCT) {
+    productsQuery.equalTo('category', selectedCategory);
+  }
   productsQuery.find().then(function (products) {
     var likes = null;
     var comments = null;
     var likeQueries = products.map(function (product) {
       var likesQuery = new Parse.Query('Likes');
-      likesQuery.equalTo('likedProduct', product);
+      likesQuery
+        .equalTo('likedProduct', product)
+        .include('likedUser');
       return likesQuery.find();
     })
 
     var commentQueries = products.map(function (product) {
       var commentsQuery = new Parse.Query('Comments');
-      commentsQuery.equalTo('product', product);
+      commentsQuery
+        .equalTo('product', product)
+        .include('commenter')
+        .descending('createdAt');
       return commentsQuery.find();
     })
 
@@ -201,7 +304,7 @@ Parse.Cloud.define('productWithRelated', function (request, response) {
         var liked = false;
         if (request.user && request.user.id) {
           likes[index].forEach(function (like) {
-            if(like.get('likedUser').id === request.user.id) liked = true;
+            if((like.get('likedUser') && like.get('likedUser').id) === request.user.id) liked = true;
           })
         }
         return {
@@ -209,7 +312,7 @@ Parse.Cloud.define('productWithRelated', function (request, response) {
           comments: comments[index],
           likes: likes[index],
           liked: liked,
-          category: 'main'
+          category: selectedCategory || 'main'
         }
       })
       response.success(result);
@@ -219,6 +322,11 @@ Parse.Cloud.define('productWithRelated', function (request, response) {
 
 Parse.Cloud.define('friendWithRelated', function (request, response) {
   if (request.user) {
+    var allFriends = [];
+    var allFriendsProducts = [];
+    var tmpFriendsFriends = [];
+    var allFriendsFriends = [];
+    var allFriendsFriendsProduct = [];
     request.user.relation('friendsRelation').query().find().then(function (friends) {
       var productQueries = friends.map(function (friend) {
         var productQuery = new Parse.Query('Products');
@@ -226,17 +334,44 @@ Parse.Cloud.define('friendWithRelated', function (request, response) {
         return productQuery.find();
       });
       Parse.Promise.when(productQueries).then(function () {
-        var products = [].slice.call(arguments);
-        var result = []
+        var friendProducts = [].slice.call(arguments);
         friends.forEach(function (friend, index) {
-          if(products[index].length !== 0) {
-            result.push({
-              friend: friend,
-              products: products[index]
-            })
+          if(friendProducts[index].length !== 0) {
+            allFriends.push(friend);
+            allFriendsProducts.push(friendProducts[index]);
           }
         })
-        response.success(result);
+        var friendsFriendQueryPromise = friends.map(function (friend) {
+          var friendRelationQuery = friend.relation('friendsRelation').query();
+          return friendRelationQuery.find();
+        });
+        return Parse.Promise.when(friendsFriendQueryPromise)
+      }).then(function () {
+          var friendsFriendsList = [].slice.call(arguments);
+          var friendsProductsQueryPromise = [];
+          friendsFriendsList.forEach(function (friendsFriends) {
+            friendsFriends.forEach(function (friendsFriend) {
+              tmpFriendsFriends.push(friendsFriend);
+              var productQuery = new Parse.Query('Products');
+              productQuery.equalTo('seller', friendsFriend);
+              friendsProductsQueryPromise.push(productQuery.find());
+            })
+          })
+          return Parse.Promise.when(friendsProductsQueryPromise);
+      }).then(function () {
+        var friendsFriendProducts = [].slice.call(arguments);
+        tmpFriendsFriends.forEach(function (tmpFriendsFriend, index) {
+          if(friendsFriendProducts[index].length !== 0) {
+            allFriendsFriends.push(tmpFriendsFriend);
+            allFriendsFriendsProduct.push(friendsFriendProducts[index]);
+          }
+        })
+        response.success({
+          allFriends: allFriends,
+          friendsProducts: allFriendsProducts,
+          allFriendsFriends: allFriendsFriends,
+          allFriendsFriendsProduct: allFriendsFriendsProduct
+        });
       })
     });
   } else {
